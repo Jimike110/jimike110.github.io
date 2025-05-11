429fa2504c43c0ddcf4405fa91407c4e96e5f666
Jimike110 <oladoyemike@gmail.com>
2025-05-12 01:17:53 +0300

    Added my Google Analytics ID

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ import {
 const settings = {
   isSplash: true, // Change this to true if you want to use the splash screen.
   useCustomCursor: true, // Change this to false if you want the good'ol cursor
-  googleTrackingID: "UA-174238252-2",
+  googleTrackingID: "G-VLL46ZEXJG",
 };
 
 //Home Page
