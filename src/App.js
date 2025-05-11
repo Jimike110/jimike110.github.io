f2625c257409adfd185a4bc122c047d915e44587
Jimike110 <oladoyemike@gmail.com>
2025-05-12 02:30:16 +0300

    Removed unused useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
