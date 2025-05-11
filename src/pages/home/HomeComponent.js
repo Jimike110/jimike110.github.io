15b3391a0423ae7e3794f3e87bab6b11ce1f5310
Jimike110 <oladoyemike@gmail.com>
2025-05-12 01:18:44 +0300

    Added a button after Skills to lead to Contact page

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -3,13 +3,36 @@ import Header from "../../components/header/Header";
 import Greeting from "../../containers/greeting/Greeting";
 import Skills from "../../containers/skills/Skills";
 import Footer from "../../components/footer/Footer";
+import { useNavigate } from "react-router";
+import { style } from "glamor";
+import { Flex } from "antd";
 
 function Home(props) {
+  const navigate = useNavigate();
+  const theme = props.theme;
+  const styles = style({
+    backgroundColor: `${theme.accentBright}`,
+    ":hover": {
+      boxShadow: `0 5px 15px ${theme.accentBright}`,
+    },
+  });
   return (
     <div>
       <Header theme={props.theme} setTheme={props.setTheme} />
       <Greeting theme={props.theme} />
       <Skills theme={props.theme} />
+      <Flex justify="center" align="center">
+        <button
+          {...styles}
+          className="button"
+          onClick={() => {
+            window.scrollTo({ top: 0 });
+            navigate("/contact");
+          }}
+        >
+          Contact & Resume
+        </button>
+      </Flex>
       <Footer theme={props.theme} />
     </div>
   );
